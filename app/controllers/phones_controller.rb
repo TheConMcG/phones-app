cf9a8f17db84c408924f41975264ae6c2170ca88
Connor <conmmcguire@gmail.com>
2021-11-29 21:30:59 -0600

    built index and show action - show action not yet dynamic

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,2 +1,13 @@
 class PhonesController < ApplicationController
+
+  def index
+    @phones = Phone.all
+    render 'index.html.erb'
+  end
+
+  def show
+    @phone = Phone.find(1)
+    render 'show.html.erb'
+  end
+
 end
